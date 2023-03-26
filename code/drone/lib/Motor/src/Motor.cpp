0860533a99140c7bdfe5fd37586201d25e191ce8
rmingon <ronanmingon69550@gmail.com>
2023-03-26 20:52:54 +0200

    class motor

--- code/drone/lib/Motor/src/Motor.cpp
+++ code/drone/lib/Motor/src/Motor.cpp
@@ -1,10 +1,66 @@
 #include "Motor.h"
 
-#define PIN_RF 10
-#define PIN_RR 11
-#define PIN_LR 12
-#define PIN_LF 8
+#define PIN_RF 25
+#define PIN_RR 26
+#define PIN_LR 27
+#define PIN_LF 32
 
 Motor::Motor() {
 
+};
+
+void Motor::init() {
+  ledcSetup(0, 1000, 8);
+  ledcAttachPin(PIN_RF, 0);
+  ledcSetup(1, 1000, 8);
+  ledcAttachPin(PIN_RR, 1);
+  ledcSetup(2, 1000, 8);
+  ledcAttachPin(PIN_LR, 2);
+  ledcSetup(3, 1000, 8);
+  ledcAttachPin(PIN_LF, 3);
+  setPwn(0);
 }
+
+void Motor::setPwn(uint8_t pwm) {
+  ledcWrite(0, pwm);
+  ledcWrite(1, pwm);
+  ledcWrite(2, pwm);
+  ledcWrite(3, pwm);
+};
+
+void Motor::front(uint8_t pwm) {
+  ledcWrite(0, pwm);
+  ledcWrite(1, pwm);
+}
+
+void Motor::left(uint8_t pwm) {
+  ledcWrite(1, pwm);
+  ledcWrite(3, pwm);
+}
+
+void Motor::right(uint8_t pwm) {
+  ledcWrite(2, pwm);
+  ledcWrite(0, pwm);
+}
+
+void Motor::back(uint8_t pwm) {
+  ledcWrite(3, pwm);
+  ledcWrite(2, pwm);
+}
+
+void Motor::rl(uint8_t pwm) {
+  ledcWrite(0, pwm);
+}
+
+void Motor::rr(uint8_t pwm) {
+  ledcWrite(2, pwm);
+}
+
+void Motor::lf(uint8_t pwm) {
+  ledcWrite(1, pwm);
+}
+
+void Motor::lr(uint8_t pwm) {
+  ledcWrite(3, pwm);
+}
+
--- code/drone/lib/Motor/src/Motor.h
+++ code/drone/lib/Motor/src/Motor.h
@@ -6,6 +6,16 @@
 class Motor {
   public:
     Motor();
+    void init();
+    void setPwn(uint8_t);
+    void front(uint8_t);
+    void left(uint8_t);
+    void right(uint8_t);
+    void back(uint8_t);
+    void rl(uint8_t);
+    void rr(uint8_t);
+    void lr(uint8_t);
+    void lf(uint8_t);
 };
 
 #endif
