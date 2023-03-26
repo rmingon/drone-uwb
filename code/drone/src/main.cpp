dbf383482548750354086f87574b91041d896110
rmingon <ronanmingon69550@gmail.com>
2023-03-26 20:53:15 +0200

    add motor logic

--- code/drone/src/main.cpp
+++ code/drone/src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <DW1000Ng.hpp>
+#include <Wire.h>
+#include <MPU6050_light.h>
+#include <Motor.h>
 
-//const uint8_t PIN_RST = 9; // reset pin
-//const uint8_t PIN_IRQ = 2; // irq pin
-const uint8_t PIN_SS = SS; // spi select pin
+MPU6050 mpu(Wire);
 
-// DEBUG packet sent status and count
 volatile unsigned long delaySent = 0;
-int16_t sentNum = 0; // todo check int type
+int16_t sentNum = 0;
+
+Motor motor = Motor();
 
 device_configuration_t DEFAULT_CONFIG = {
     false,
@@ -28,7 +30,6 @@ void transmit() {
   Serial.print("Transmitting packet ... #"); Serial.println(sentNum);
   String msg = "Hello DW1000Ng, it's #"; msg += sentNum;
   DW1000Ng::setTransmitData(msg);
-  delay(1000);
   DW1000Ng::startTransmit(TransmitMode::IMMEDIATE);
   delaySent = millis();
   while(!DW1000Ng::isTransmitDone()) {}
@@ -37,13 +38,43 @@ void transmit() {
 }
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("### DW1000Ng-arduino-sender-test ###"));
-  DW1000Ng::initializeNoInterrupt(PIN_SS);
-  Serial.println(F("DW1000Ng initialized ..."));
+  Serial.begin(115200);
+  Wire.begin();
+  
+  mpu.begin();
+  mpu.calcOffsets();
 
+  motor.init();
+
+  while(1) {
+    mpu.update();
+    int x = map(mpu.getAngleX(), 0, 100, 0, 20);
+    int y = map(mpu.getAngleY(), 0, 100, 0, 20);
+    if (-y+x > 0) {
+      motor.lf(-y+x);
+    } else {
+      motor.lf(0);
+    }
+    if (y+x > 0) {
+      motor.lr(y+x);
+    } else {
+      motor.lr(0);
+    }
+    if (-y+-x > 0) {
+      motor.rl(-y+-x);
+    } else {
+      motor.rl(0);
+    }
+    if (y+-x > 0) {
+      motor.rr(y+-x);
+    } else {
+      motor.rr(0);
+    }
+  }
+
+
+  DW1000Ng::initializeNoInterrupt(5);
   DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
-	//DW1000Ng::applyInterruptConfiguration(DEFAULT_INTERRUPT_CONFIG);
 
   DW1000Ng::setDeviceAddress(5);
   DW1000Ng::setNetworkId(10);
@@ -60,8 +91,6 @@ void setup() {
   Serial.print("Network ID & Device Address: "); Serial.println(msg);
   DW1000Ng::getPrintableDeviceMode(msg);
   Serial.print("Device mode: "); Serial.println(msg);
-
-  transmit();
 }
 
 void loop() {
@@ -69,4 +98,5 @@ void loop() {
     Serial.print("ARDUINO delay sent [ms] ... "); Serial.println(millis() - delaySent);
     uint64_t newSentTime = DW1000Ng::getTransmitTimestamp();
     Serial.print("Processed packet ... #"); Serial.println(sentNum);
+
 }
