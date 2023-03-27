ad4e32031712e59b27f8603b07de87cdc224e96b
rmingon <ronanmingon69550@gmail.com>
2023-03-27 08:35:29 +0200

    add bmp280

--- code/drone/src/main.cpp
+++ code/drone/src/main.cpp
@@ -4,8 +4,11 @@
 #include <Wire.h>
 #include <MPU6050_light.h>
 #include <Motor.h>
+#include <Adafruit_BMP280.h>
 
 MPU6050 mpu(Wire);
+
+Adafruit_BMP280 bmp;
 
 volatile unsigned long delaySent = 0;
 int16_t sentNum = 0;
@@ -46,32 +49,14 @@ void setup() {
 
   motor.init();
 
-  while(1) {
-    mpu.update();
-    int x = map(mpu.getAngleX(), 0, 100, 0, 20);
-    int y = map(mpu.getAngleY(), 0, 100, 0, 20);
-    if (-y+x > 0) {
-      motor.lf(-y+x);
-    } else {
-      motor.lf(0);
-    }
-    if (y+x > 0) {
-      motor.lr(y+x);
-    } else {
-      motor.lr(0);
-    }
-    if (-y+-x > 0) {
-      motor.rl(-y+-x);
-    } else {
-      motor.rl(0);
-    }
-    if (y+-x > 0) {
-      motor.rr(y+-x);
-    } else {
-      motor.rr(0);
-    }
-  }
-
+  bmp.begin(0x76);
+  
+  bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X1,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
+                  
 
   DW1000Ng::initializeNoInterrupt(5);
   DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
@@ -94,9 +79,47 @@ void setup() {
 }
 
 void loop() {
-    transmit();
-    Serial.print("ARDUINO delay sent [ms] ... "); Serial.println(millis() - delaySent);
-    uint64_t newSentTime = DW1000Ng::getTransmitTimestamp();
-    Serial.print("Processed packet ... #"); Serial.println(sentNum);
+  transmit();
+  Serial.print("ARDUINO delay sent [ms] ... "); Serial.println(millis() - delaySent);
+  uint64_t newSentTime = DW1000Ng::getTransmitTimestamp();
+  Serial.print("Processed packet ... #"); Serial.println(sentNum);
 
+  mpu.update();
+  int x = map(mpu.getAngleX(), 0, 100, 0, 20);
+  int y = map(mpu.getAngleY(), 0, 100, 0, 20);
+  if (-y+x > 0) {
+    motor.lf(-y+x);
+  } else {
+    motor.lf(0);
+  }
+  if (y+x > 0) {
+    motor.lr(y+x);
+  } else {
+    motor.lr(0);
+  }
+  if (-y+-x > 0) {
+    motor.rl(-y+-x);
+  } else {
+    motor.rl(0);
+  }
+  if (y+-x > 0) {
+    motor.rr(y+-x);
+  } else {
+    motor.rr(0);
+  }
+  
+  if (bmp.takeForcedMeasurement()) {
+    // can now print out the new measurements
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+  }
 }
