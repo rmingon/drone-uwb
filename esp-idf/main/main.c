b282ab69a79505348617851384a9e4ff1e7737c5
rmingon <ronanmingon69550@gmail.com>
2023-09-25 09:12:53 +0200

    add motor define pin

--- esp-idf/main/main.c
+++ esp-idf/main/main.c
@@ -4,6 +4,21 @@
 #include "led_strip.h"
 #include "esp_log.h"
 #include "esp_err.h"
+#include "freertos/queue.h"
+#include "driver/ledc.h"
+
+typedef struct {
+    uint8_t l;
+    uint8_t r;
+    uint8_t b;
+    uint8_t t;
+} motor_t;
+TaskHandle_t TaskMotorControl;
+QueueHandle_t MotorSpeedQueue;
+#define LEFT_MOTOR_PIN 32
+#define RIGHT_MOTOR_PIN 27
+#define TOP_MOTOR_PIN 26
+#define BOTTOM_MOTOR_PIN 25
 
 // GPIO assignment
 #define LED_STRIP_BLINK_GPIO  14
@@ -18,7 +33,67 @@
 
 static const char *TAG = "example";
 
-led_strip_handle_t configure_led(void)
+QueueHandle_t queue1;
+
+ledc_timer_config_t motor_timer = {
+    .speed_mode = LEDC_LOW_SPEED_MODE,
+    .timer_num  = LEDC_TIMER_0,
+    .duty_resolution = LEDC_TIMER_13_BIT,
+    .freq_hz = 1000,
+    .clk_cfg = LEDC_AUTO_CLK
+};
+
+ledc_channel_config_t motor_channel[4];
+
+static void pwm_init(void)
+{
+    ESP_ERROR_CHECK(ledc_timer_config(&motor_timer));
+
+    motor_channel[0].channel = LEDC_CHANNEL_0;
+    motor_channel[0].gpio_num = LEFT_MOTOR_PIN;
+
+    motor_channel[1].channel = LEDC_CHANNEL_1;
+    motor_channel[1].gpio_num = RIGHT_MOTOR_PIN;
+
+    motor_channel[2].channel = LEDC_CHANNEL_2;
+    motor_channel[2].gpio_num = TOP_MOTOR_PIN;
+
+    motor_channel[3].channel = LEDC_CHANNEL_3;
+    motor_channel[3].gpio_num = BOTTOM_MOTOR_PIN;
+
+    for (int i = 0; i < 4; i++)
+    {
+    	motor_channel[i].speed_mode = LEDC_LOW_SPEED_MODE;
+    	motor_channel[i].timer_sel = LEDC_TIMER_0;
+    	motor_channel[i].intr_type = LEDC_INTR_DISABLE;
+    	motor_channel[i].duty = 0;
+    	motor_channel[i].hpoint = 0;
+
+        ESP_ERROR_CHECK(ledc_channel_config(&motor_channel[i]));
+    }
+}
+
+void motor_control(void)
+{
+	while(1) {
+	    uint32_t red_duty = 8191 * color.r / 255;
+	    uint32_t green_duty = 8191 * color.g / 255;
+	    uint32_t blue_duty = 8191 * color.b / 255;
+
+	    ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, red_duty));
+	    ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+
+	    ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, green_duty));
+	    ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1));
+
+	    ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2, blue_duty));
+	    ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_2));
+
+	}
+
+}
+
+led_strip_handle_t led_init(void)
 {
     // LED strip general initialization, according to your led board design
     led_strip_config_t strip_config = {
