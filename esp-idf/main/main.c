de9685f72c0771ee228184930a2fd5a5b06bbbd9
rmingon <ronanmingon69550@gmail.com>
2023-09-25 18:41:36 +0200

    add wifi

--- esp-idf/main/main.c
+++ esp-idf/main/main.c
@@ -6,19 +6,35 @@
 #include "esp_err.h"
 #include "freertos/queue.h"
 #include "driver/ledc.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "mqtt_client.h"
+#include "esp_tls.h"
+#include "esp_ota_ops.h"
 
-typedef struct {
+#include <sys/param.h>
+struct color_t {
     uint8_t r;
     uint8_t g;
     uint8_t b;
-} color_t;
+};
 
-typedef struct {
-    uint8_t l;
-    uint8_t r;
-    uint8_t b;
-    uint8_t t;
-} motor_t;
+struct motor_t {
+    int l;
+    int r;
+    int b;
+    int t;
+};
+
+typedef struct
+{
+  float ax; float ay; float az;
+  float gx; float gy; float gz;
+  float tp;
+} Data_t;
 
 TaskHandle_t TaskMotorControl;
 
@@ -43,6 +59,90 @@ QueueHandle_t MotorSpeedQueue;
 static const char *TAG = "example";
 
 QueueHandle_t queue1;
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY  3
+
+
+static void event_handler(void* arg, esp_event_base_t event_base,int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+}
 
 ledc_timer_config_t motor_timer = {
     .speed_mode = LEDC_LOW_SPEED_MODE,
@@ -129,22 +229,34 @@ led_strip_handle_t led_init(void)
 
 void app_main(void)
 {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+
+    MotorSpeedQueue = xQueueCreate(1, sizeof( struct motor_t ));
+
     led_strip_handle_t led_strip = led_init();
     pwm_init();
+    xTaskCreate(motor_control, "TaskMotorControl", 5000, NULL, 1, &TaskMotorControl);
+
+/*
+    while (1) {
+
     bool led_on_off = false;
 
-    ESP_LOGI(TAG, "Start blinking LED strip");
-    while (1) {
+
         if (led_on_off) {
-            /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
             for (int i = 0; i < LED_STRIP_LED_NUMBERS; i++) {
                 ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 50, 5, 5));
             }
-            /* Refresh the strip to send data */
             ESP_ERROR_CHECK(led_strip_refresh(led_strip));
             ESP_LOGI(TAG, "LED ON!");
         } else {
-            /* Set all LED off to clear all pixels */
             ESP_ERROR_CHECK(led_strip_clear(led_strip));
             ESP_LOGI(TAG, "LED OFF!");
         }
@@ -152,4 +264,5 @@ void app_main(void)
         led_on_off = !led_on_off;
         vTaskDelay(pdMS_TO_TICKS(500));
     }
+    */
 }
