c83079657b1046a493df7bfc816f787dcb0ad20c
rmingon <ronanmingon69550@gmail.com>
2023-09-27 09:47:32 +0200

    change i2c to last api

--- esp-idf/main/i2c.c
+++ esp-idf/main/i2c.c
@@ -11,9 +11,11 @@
 #include "esp_log.h"
 #include "freertos/task.h"
 #include "i2c.h"
+#include "freertos/FreeRTOS.h"
 
 #define SDA_PIN GPIO_NUM_21
 #define SCL_PIN GPIO_NUM_22
+#define I2C_CHANNEL 0
 
 void i2cInit()
 {
@@ -25,57 +27,38 @@ void i2cInit()
 		.scl_pullup_en = GPIO_PULLUP_ENABLE,
 		.master.clk_speed = 400000,
 	};
-	i2c_param_config(I2C_NUM_0, &conf);
+	i2c_param_config(I2C_CHANNEL, &conf);
 
-	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_CHANNEL, I2C_MODE_MASTER, 0, 0, 0));
 }
 
-int i2cRead(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+esp_err_t i2cWrite(uint8_t i2c_addr, uint8_t reg_addr, uint8_t reg_data)
 {
-
-    int iError;
-    i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
-    i2c_master_start(cmd_handle);
-    i2c_master_write_byte(cmd_handle, (i2c_addr << 1) | I2C_MASTER_WRITE, true);
-    i2c_master_write_byte(cmd_handle, reg_addr, true);
-    i2c_master_start(cmd_handle);
-    i2c_master_write_byte(cmd_handle, (i2c_addr << 1) | I2C_MASTER_READ, true);
-    if (length > 1) {
-        i2c_master_read(cmd_handle, reg_data, length - 1, I2C_MASTER_ACK);
-    }
-    i2c_master_read_byte(cmd_handle, reg_data + length - 1, I2C_MASTER_NACK);
-    i2c_master_stop(cmd_handle);
-    esp_err_t esp_err = i2c_master_cmd_begin(I2C_NUM_0, cmd_handle, 1000 / portTICK_PERIOD_MS);
-
-    if (esp_err == ESP_OK) {
-        iError = 0;
-    } else {
-        iError = -1;
-    }
-
-    i2c_cmd_link_delete(cmd_handle);
-
-
-    return iError;
+    uint8_t write_buf[2] = {reg_addr, reg_data};
+    return i2c_master_write_to_device(I2C_CHANNEL, i2c_addr, write_buf, sizeof(write_buf), 1000);
 }
 
-int i2cWrite(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+esp_err_t i2cRead(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
 {
-    /* Implement the I2C write routine according to the target machine. */
-    int8_t iError;
-    esp_err_t esp_err;
-    i2c_cmd_handle_t cmd_handle = i2c_cmd_link_create();
-    i2c_master_start(cmd_handle);
-    i2c_master_write_byte(cmd_handle, (i2c_addr << 1) | I2C_MASTER_WRITE, true);
-    i2c_master_write_byte(cmd_handle, reg_addr, true);
-    i2c_master_write(cmd_handle, reg_data, length,true);
-    i2c_master_stop(cmd_handle);
-    esp_err = i2c_master_cmd_begin(I2C_NUM_0, cmd_handle, 1000 / portTICK_PERIOD_MS);
-    if (esp_err == ESP_OK) {
-        iError = 0;
-    } else {
-        iError = -1;
-    }
-    i2c_cmd_link_delete(cmd_handle);
-    return iError;
+	return i2c_master_write_read_device(I2C_CHANNEL, i2c_addr, &reg_addr, 1, reg_data, length, 1000);
+}
+
+
+void i2cScan() {
+    printf("i2c scan: \n");
+	for (uint8_t i = 1; i < 127; i++)
+	{
+		int ret;
+		i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+		i2c_master_start(cmd);
+		i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1);
+		i2c_master_stop(cmd);
+		ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 100);
+		i2c_cmd_link_delete(cmd);
+
+		if (ret == ESP_OK)
+		{
+			printf("Found device at: 0x%2x\n", i);
+		}
+	}
 }
--- esp-idf/main/i2c.h
+++ esp-idf/main/i2c.h
@@ -8,11 +8,16 @@
 #ifndef MAIN_I2C_H_
 #define MAIN_I2C_H_
 
+#include "esp_err.h"
+#include "esp_log.h"
+
 void i2cInit();
 
-int i2cRead(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+esp_err_t i2cRead(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
 
-int i2cWrite(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+esp_err_t i2cWrite(uint8_t i2c_addr, uint8_t reg_addr, uint8_t reg_data);
+
+void i2cScan();
 
 
 #endif /* MAIN_I2C_H_ */
