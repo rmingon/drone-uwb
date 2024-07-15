5b63d559f476ee5f18a93878b4caacd6454efecd
rmingon <ronanmingon69550@gmail.com>
2024-07-15 16:45:58 +0200

    add methods

--- code/server/index.ts
+++ code/server/index.ts
@@ -1,38 +1,41 @@
-import { filter, Subject } from "rxjs";
+import { filter, interval, Subject } from "rxjs";
 import { Anchor } from "./anchor";
 import { Drone } from "./drone";
 
-interface DronePosition {
-
+export interface DronePosition {
+  pitch: string;
+  roll: string;
+  yaw: string;
+  throttle: number;
 }
 
-interface DroneConnection {
+interface DroneConnection {}
 
-}
-
-interface AnchorConnection {
-
-}
+interface AnchorConnection {}
 
 interface AnchorRange {
-
+  uniq: string; // drone uniq
+  quality: string;
+  power: string;
 }
 
-type typeOfData = AnchorConnection | DroneConnection | DronePosition | AnchorRange;
+type DataType = DroneConnection | AnchorConnection | AnchorRange | DronePosition
 
 interface Data<T> {
   uniq: string;
   data: T
   type: string
+  ip: string
 }
 
-const listener = new Subject<Data<typeOfData>>();
-const server = await Bun.udpSocket({
+const listener = new Subject<any>();
+await Bun.udpSocket({
   port: 7051,
   socket: {
     data(socket, buf, port, addr) {
-      console.log(`message from ${addr}:${port}:`)
-      listener.next(JSON.parse(buf.toString()));
+      const data : Data<DataType> = JSON.parse(buf.toString())
+      data.ip = addr
+      listener.next(data);
     }
   }
 })
@@ -45,10 +48,24 @@ const $dronePosition = listener.pipe(filter<Data<DronePosition>>(data => data.ty
 const anchors = new Map<string, Anchor>()
 const drones = new Map<string, Drone>()
 
-$anchorConnection.subscribe(({uniq}) => {
-  anchors.set(uniq, new Anchor(uniq))
+$anchorConnection.subscribe(({uniq, ip}) => {
+  anchors.set(uniq, new Anchor(uniq, ip))
 })
 
-$droneConnection.subscribe(({uniq}) => {
-  drones.set(uniq, new Drone(uniq))
+$anchorRange.subscribe(({uniq, ip}) => {
+  const anchor = anchors.get(uniq)
+  if (anchor) 
+    anchor.setRange()
 })
+
+$droneConnection.subscribe(({uniq, ip}) => {
+  drones.set(uniq, new Drone(uniq, ip))
+})
+
+$dronePosition.subscribe((position) => {
+  const drone = drones.get(position.uniq)
+  if (drone)
+    drone.setPosition(position.data)
+})
+
+console.log("GO")
