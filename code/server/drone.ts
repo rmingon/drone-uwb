b926b7d9b09f3726bc13e8f5f77aea854b43d9ed
rmingon <ronanmingon69550@gmail.com>
2024-07-15 16:46:25 +0200

    add reboot + setPosition

--- code/server/drone.ts
+++ code/server/drone.ts
@@ -1,15 +1,29 @@
-import {WebSocket} from "ws";
+import type { DronePosition } from ".";
 
-
+const client = await Bun.udpSocket({});
 
 export class Drone {
   id: string = "";
+  ip: string = "";
 
-  x: number = 0
-  y: number = 0
-  altitude: number = 0
+  pitch: string = ""
+  roll: string = ""
+  yaw: string = ""
+  throttle: number = 0
 
-  constructor(id: string) {
+  constructor(id: string, ip: string) {
     this.id = id
+    this.ip = ip
+  }
+
+  setPosition({pitch, roll, yaw, throttle}: DronePosition) {
+    this.pitch = pitch
+    this.roll = roll
+    this.yaw = yaw
+    this.throttle = throttle
+  }
+
+  reboot() {
+    client.send(JSON.stringify({reboot: true}), 7051, this.ip);
   }
 }
