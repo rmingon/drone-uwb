611092e3924e2747c83ee798582c20f0e71a99d7
rmingon <ronanmingon69550@gmail.com>
2024-07-17 21:33:47 +0200

    add first test

---
+++ code/server/test/anchor.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test, describe } from "bun:test";
+import { Anchor } from "../anchor";
+
+
+describe("Anchor test", () => {
+    
+    test("Should instance the class", () => {
+        const id = "1"
+        const ip = "192.168.1.1"
+        const anchor = new Anchor('1', '192.168.1.1')
+        expect(anchor.ip).toBe(ip)
+        expect(anchor.id).toBe(id)
+    })
+
+    test("Should call reboot", () => {
+        
+    })
+})
