b899aa8764ed4adea9a55cdc14356017e09ca088
rmingon <ronanmingon69550@gmail.com>
2025-01-17 19:09:36 +0100

    false credential wifi

---
+++ drone_with_esc/software/src/secrets.h
@@ -0,0 +1,9 @@
+// credentials.h
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Replace with your actual SSID and password
+const char* WIFI_SSID = "Your_SSID";
+const char* WIFI_PASSWORD = "Your_PASSWORD";
+
+#endif
